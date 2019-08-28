333ae88389d90337a6dcaeaa3ecf3e6dfaa27276
hanasari <Calvin@Daejongui-iMac.local>
2019-08-28 19:50:57 +0900

    being test

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const gulp = require('gulp');
 const run = require('gulp-run');
+// const shell = require('gulp-shell');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const icomoon = require('../lib/export');
@@ -25,7 +26,7 @@ async function styles(){
         .pipe(gulp.dest('./src/css'));
 }
 
-async function glyph(){
+function glyph(){
     let fontName, icomoonZipFile, preProcessorPath, cssPath, fontPath, docsPath, ligaPath, minifyCss;
 
     fontName = 'glyphicon';
@@ -38,10 +39,16 @@ async function glyph(){
     minifyCss = 'Y';
 
     var cmd = new run.Command(
-        `node ${path.resolve('../lib/builder export')} ${fontName} ${icommonZipFile} ${preProcessorPath} ${cssPath} ${fontPath} ${docsPath} ${ligaPath}`,
-        {verbosity: 0}
+        `node ${path.resolve('../lib/builder export ')} ${fontName} ${icommonZipFile} ${preProcessorPath} ${cssPath} ${fontPath} ${docsPath} ${ligaPath}  --force`,
+        {verbosity: 3}
     );
-    cmd.exec(`Y | ${minifyCss}`);
+    return cmd.exec();
+
+    // return run(`node ${path.resolve('../lib/builder export')} ${fontName} ${icommonZipFile} ${preProcessorPath} ${cssPath} ${fontPath} ${docsPath} ${ligaPath}`).exec().pipe();
+
+    // shell.task(`node ${path.resolve('../lib/builder export')} ${fontName} ${icommonZipFile} ${preProcessorPath} ${cssPath} ${fontPath} ${docsPath} ${ligaPath}`);
+
+    // return gulp.src('../lib/builder.js').pipe(run(`node ${path.resolve('../lib/builder export')} ${fontName} ${icommonZipFile} ${preProcessorPath} ${cssPath} ${fontPath} ${docsPath} ${ligaPath}`).exec());
 }
 
 async function watch(){
